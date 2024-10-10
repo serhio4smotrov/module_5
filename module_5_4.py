c606165b22c9724e945065b94e0016254e9e1064
serhio4smotrov <ser19061995@gmail.com>
2024-10-10 14:09:12 +0300

    initial

--- module_5_4.py
+++ module_5_4.py
@@ -1,7 +1,13 @@
 class House:
+    houses_history = []
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
     def go_to(self, new_floor):
         if new_floor > self.number_of_floors:
             print('Такого этажа не существует')
@@ -44,23 +50,13 @@ class House:
             return self
 
 
-
-h1 = House('ЖК Небо', 10)
-h2 = House('ЖК Горьковский', 20)
-print(h1)
-print(h2)
-print(h1 == h2) # __eq__
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
-h1 += 10 # __iadd__
-print(h1)
-h2 = 10 + h2 # __radd__
-print(h2)
-h2 = h2 - 10 # __sub__
-print(h2)
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+# Удаление объектов
+del h2
+del h3
+print(House.houses_history)
